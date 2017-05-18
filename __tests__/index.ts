7b540031a3b82e94f43cb439d412599c422a29b2
Robert Rees <shudderfix@gmail.com>
2017-05-18 22:52:43 +0100

    Adds a first data table and associated code

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -2,5 +2,16 @@ import * as rtg from "../src/index.ts";
 
 it('Should generate something', () => {
 	const town = rtg.generate();
-	expect(town.size).toBe(1);
+	expect(town.size).toBe(2);
+});
+
+it('Should contain the expected town elements', () => {
+	const keys = Array.from(rtg.generate().keys());
+	console.log(keys);
+	const expectedKeys = [
+		"Population",
+		"Ruling attitude",
+	];
+
+	expect(keys).toEqual(expect.arrayContaining(expectedKeys));
 });
--- src/town.ts
+++ src/town.ts
@@ -1,9 +1,14 @@
 
 import {Town} from './aliases';
 
+import * as classicData from './data/classic';
+
+import * as selectors from './selectors';
+
 export function classic(): Town {
 	const townBuilders = [
-		(town: Town) => town.set('Population', {name: "Population", value: "Village"})
+		(town: Town) => town.set('Population', {name: "Population", value: "Village"}),
+		(town: Town) => selectors.chooseOne('Ruling attitude', classicData.rulingAttitudes, town),
 	];
 
 	return townBuilders.reduce((town, builder) => builder(town), new Map());
